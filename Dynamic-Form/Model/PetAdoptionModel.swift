1d0f1a40a2a077e8685c5f387fcc42056328bb81
Henry Chukwu <henrychukwu13@gmail.com>
2020-05-28 15:15:44 +0100

    populate json file

---
+++ Dynamic-Form/Model/PetAdoptionModel.swift
@@ -0,0 +1,32 @@
+import Foundation
+
+struct PetAdoptionModel: Decodable {
+    let id: String
+    let name: String
+    let pages: [Pages]
+}
+
+struct Pages: Decodable {
+    let label: String
+    let sections: [Sections]
+}
+
+struct Sections: Decodable {
+    let label: String
+    let elements: [Elements]
+}
+
+struct Elements: Decodable {
+    let type: String
+    let file: String
+    let unique_id: String
+    let rules: [Rules]
+}
+
+struct Rules: Decodable {
+    let condition: String
+    let value: String
+    let action: String
+    let otherwise: String
+    let targets: [String]
+}
