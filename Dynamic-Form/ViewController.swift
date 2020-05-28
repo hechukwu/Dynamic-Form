6117b248a4355a339300b5ddd8d7a2f4be7b3902
Henry Chukwu <henrychukwu13@gmail.com>
2020-05-28 13:57:36 +0100

    Initial Commit

---
+++ Dynamic-Form/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dynamic-Form
+//
+//  Created by Henry Chukwu on 28/05/2020.
+//  Copyright © 2020 Henry Chukwu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Dynamic-FormTests/Dynamic_FormTests.swift
@@ -0,0 +1,34 @@
+//
+//  Dynamic_FormTests.swift
+//  Dynamic-FormTests
+//
+//  Created by Henry Chukwu on 28/05/2020.
+//  Copyright © 2020 Henry Chukwu. All rights reserved.
+//
+
+import XCTest
+@testable import Dynamic_Form
+
+class Dynamic_FormTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
